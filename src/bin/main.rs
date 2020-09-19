77b403bfbcb467b6a01a50ac969320250b839579
Nyefari <nyefari@gmail.com>
2020-09-18 20:50:07 -0700

    no message

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,98 +1,131 @@
 use bevy::{
     prelude::*,
     render::{
-        mesh::shape,
-        pipeline::{DynamicBinding, PipelineDescriptor, PipelineSpecialization, RenderPipeline},
-        render_graph::{base, AssetRenderResourcesNode, RenderGraph},
-        renderer::RenderResources,
-        shader::{ShaderStage, ShaderStages},
+        mesh::{shape,VertexAttribute},
     },
+    input::mouse::{
+        MouseWheel,
+        MouseScrollUnit
+    }
 };
+use bounded_planet::camera::*;
 
+// The thresholds for window edge.
+const CURSOR_H_THRESHOLD: f32 = 0.55;
+const CURSOR_V_THRESHOLD: f32 = 0.42;
 
+/// The stage at which the [`CameraBP`] cache is either updated or used to fill
+/// in the action cache now.
+const CAM_CACHE_UPDATE: &'static str = "push_cam_update";
+
+#[derive(Default)]
+struct MoveCam {
+    right: Option<f32>,
+    forward: Option<f32>
+}
 
 fn main() {
     App::build()
+        .init_resource::<MoveCam>()
         .add_resource(Msaa { samples: 4 })
         .add_default_plugins()
-        .add_asset::<LandMaterial>()
+        .add_plugin(CameraBPPlugin {
+            geo: UniversalGeometry::Plane {
+                origin: Translation::identity(),
+                normal: Vec3::new(0.0, 1.0, 0.0)
+            },
+            ..Default::default()
+        })
         .add_startup_system(setup.system())
+        .add_system_to_stage(stage::EVENT_UPDATE, act_camera_on_window_edge.system())
+        .add_system_to_stage(stage::EVENT_UPDATE, act_on_scroll_wheel.system())
+        .add_stage_after(stage::EVENT_UPDATE, CAM_CACHE_UPDATE)
+        .add_system_to_stage(CAM_CACHE_UPDATE, use_or_update_action_cache.system())
         .run();
 }
-
-#[derive(RenderResources, Default)]
-struct LandMaterial {
-    pub color: Color,
-}
-
-const VERTEXSHADER: &str = include_str!("../media/shaders/land.vert");
-const FRAGSHADER: &str = include_str!("../media/shaders/land.frag");
 
 /// set up a simple 3D scene with landscape?
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     mut meshes: ResMut<Assets<Mesh>>,
+    mut textures: ResMut<Assets<Texture>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
-    mut pipelines: ResMut<Assets<PipelineDescriptor>>,
-    mut shaders: ResMut<Assets<Shader>>,
-    mut shader_materials: ResMut<Assets<LandMaterial>>,
-    mut render_graph: ResMut<RenderGraph>, 
 ) {
-    let land_pipeline = pipelines.add(PipelineDescriptor::default_config(ShaderStages {
-        vertex: shaders.add(Shader::from_glsl(ShaderStage::Vertex,VERTEXSHADER)),
-        fragment: Some(shaders.add(Shader::from_glsl(ShaderStage::Fragment, FRAGSHADER))),
-    }));
-
-    render_graph.add_system_node("land_material", AssetRenderResourcesNode::<LandMaterial>::new(true),);
-
-    render_graph
-        .add_node_edge("land_material", base::node::MAIN_PASS)
+    let land_texture_handle = asset_server
+        .load_sync(&mut textures, "src/media/Height.png")
         .unwrap();
+    
+    std::thread::sleep(std::time::Duration::from_secs_f32(2.0));
 
-    let _land_material = shader_materials.add(LandMaterial {
-        color: Color::rgb(0.0, 0.8, 0.0),
-    });
+    let land_texture = textures.get(&land_texture_handle).unwrap().clone();
+    let land_size = land_texture.size;
+    let land_height: Vec<u8> = land_texture.data.iter()
+        .copied()
+        .enumerate()
+        .filter(|(i, _): &(usize, u8)| i % 4 == 2)
+        .unzip::<_, u8, Vec<usize>, _>()
+        .1;
+
+    let mut land_positions = Vec::with_capacity((land_size.x() * land_size.y()) as usize);
+    let mut land_normals = Vec::with_capacity((land_size.x() * land_size.y()) as usize);
+    let mut land_uvs = Vec::with_capacity((land_size.x() * land_size.y()) as usize);
+
+    let mut land_indices = Vec::with_capacity(((land_size.x() - 1.0) * (land_size.y() - 1.0)) as usize);
+
+    let land_scale = land_size.x() as i32;
+
+    for z in 0..(land_size.y() as i32) {
+        for x in 0..land_scale {
+            
+            let i: u32 = x as u32 + z as u32 * land_size.y() as u32;
+            
+            land_positions.push([x as f32 * 0.2, land_height[i as usize] as f32 / 128.0, z as f32 * 0.2]);
+            land_normals.push([0.0, 1.0, 0.0]);
+            land_uvs.push([x as f32 / land_size.x(), z as f32 / land_size.y()]);
 
 
-    for x in -15..15 {
-        for y in -15..15 {
-            let scale = 5.0;
-            let land_mesh = PbrComponents{
-                mesh: asset_server.load("src/media/land.gltf").unwrap(),
-                render_pipelines: RenderPipelines::from_pipelines(vec![RenderPipeline::specialized(
-                    land_pipeline,
-                    PipelineSpecialization {
-                        dynamic_bindings: vec![
-                            // Transform
-                            DynamicBinding {
-                                bind_group: 1,
-                                binding: 0,
-                            },
-                        ],
-                        ..Default::default()           
-                    },
-                )]),
-                translation: Translation::new(x as f32, 0.0, y as f32),
-                scale: Scale(scale),
-                material: materials.add(Color::rgb(0.5, 0.4, 0.3).into()),
-                ..Default::default()
-            };
+            if x != land_scale - 1 && z != land_scale - 1 {
+                land_indices.push(i);
+                land_indices.push(i + land_scale as u32); 
+                land_indices.push(i + land_scale as u32 + 1);
 
-            commands.spawn(land_mesh).with(_land_material);  
+                land_indices.push(i);
+                land_indices.push(i + land_scale as u32 + 1);
+                land_indices.push(i + 1);
+            }
         }
     }
 
+    let land_mesh = Mesh {
+        primitive_topology: bevy::render::pipeline::PrimitiveTopology::TriangleList,
+        attributes: vec![
+            VertexAttribute::position(land_positions),
+            VertexAttribute::normal(land_normals),
+            VertexAttribute::uv(land_uvs),
+        ],
+        indices: Some(land_indices),
+    };
+
+    
+    commands.spawn(PbrComponents {
+        mesh: meshes.add(land_mesh),
+        material: materials.add(StandardMaterial {
+            albedo_texture: Some(land_texture_handle),
+            shaded: false,
+            ..Default::default()
+        }),
+        translation: Translation::new(4.0, 1.5, 4.0),
+        ..Default::default()
+    });
+
     // add entities to the world
     commands
-        // sphere
+        // cube
         .spawn(PbrComponents {
-            mesh: meshes.add(Mesh::from(shape::Icosphere {
-                subdivisions: 4,
-                radius: 2.0,
-            })),
-            material: materials.add(Color::rgb(0.1, 0.4, 0.8).into()),
-            translation: Translation::new(4.0, 1.5, 4.0),
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.5, 0.4, 0.3).into()),
+            translation: Translation::new(-land_scale as f32, 1.0, -land_scale as f32),
             ..Default::default()
         })
         // light
@@ -102,17 +135,76 @@ fn setup(
         })
         // camera
         .spawn(Camera3dComponents {
-            transform: Transform::new_sync_disabled(Mat4::face_toward(
-                Vec3::new(-8.0, 20.0, 15.0),
-                Vec3::new(0.0, 0.0, 0.0),
-                Vec3::new(0.0, 1.0, 0.0),
-            )),
+            translation: Translation::new(20.0, 20.0, 20.0),
+            rotation: Rotation::from_rotation_xyz(-0.75, 2.7, 0.0),
+            ..Default::default()
+        }).with(CameraBPConfig {
+            forward_weight: -0.06,
+            back_weight: 0.06,
+            left_weight: -0.06,
+            right_weight: 0.06,
             ..Default::default()
         });
 }
 
+/// Pushes camera actions based upon mouse movements near the window edge.
+fn act_camera_on_window_edge(
+    wins: Res<Windows>,
+    pos: Res<Events<CursorMoved>>,
+    mut mcam: ResMut<MoveCam>
+) {
+    if let Some(e) = pos.get_reader().find_latest(&pos, | e | e.id.is_primary()) {
+        let (mut mouse_x, mut mouse_y) = (e.position.x(), e.position.y());
+        let window = wins.get(e.id).expect("Couldn't get primary window.");
+        let (window_x, window_y) = (window.width as f32, window.height as f32);
+        
+        // map (mouse_x, mouse_y) into [-1, 1]^2
+        mouse_x /= window_x / 2.0;
+        mouse_y /= window_y / 2.0;
+        mouse_x -= 1.0;
+        mouse_y -= 1.0;
+        let angle = mouse_x.atan2(mouse_y);
+        let (ax, ay) = (angle.sin(), angle.cos());
+        let in_rect = (-CURSOR_H_THRESHOLD <= mouse_x && mouse_x <= CURSOR_H_THRESHOLD)
+            && (-CURSOR_V_THRESHOLD <= mouse_y && mouse_y <= CURSOR_V_THRESHOLD);
+        
+        if !in_rect && ax.is_finite() && ay.is_finite() {
+            mcam.right = Some(ax);
+            mcam.forward = Some(ay);
+        } else {
+            mcam.right = None;
+            mcam.forward = None;
+        }
+    }
+}
 
+/// Pushes camera actions based upon scroll wheel movement.
+fn act_on_scroll_wheel(
+    mouse_wheel: Res<Events<MouseWheel>>,
+    mut acts: ResMut<Events<CameraBPAction>>
+) {
+    for mw in mouse_wheel.get_reader().iter(&mouse_wheel) {
+        /// If scrolling units are reported in lines rather than pixels,
+        /// multiply the returned horizontal scrolling amount by this.
+        const LINE_SIZE: f32 = 14.0;
+        let w = mw.y.abs() * if let MouseScrollUnit::Line = mw.unit { LINE_SIZE } else { 1.0 };
 
+        if mw.y > 0.0 {
+            acts.send(CameraBPAction::ZoomIn(Some(w)))
+        } else if mw.y < 0.0 {
+            acts.send(CameraBPAction::ZoomOut(Some(w)))
+        }
+    }
+}
 
+/// Depending on `dirty`, either update the local `cache` or fill the event
+/// queue for [`CameraBPAction`] with the locally cached copy.
+fn use_or_update_action_cache(mcam: Res<MoveCam>, mut acts: ResMut<Events<CameraBPAction>>) {
+    if let Some(w) = mcam.right {
+        acts.send(CameraBPAction::MoveRight(Some(w)))
+    }
 
-
+    if let Some(w) = mcam.forward {
+        acts.send(CameraBPAction::MoveForward(Some(w)))
+    }
+}
--- src/media/shaders/land.vert
+++ src/media/shaders/land.vert
@@ -1,28 +1,27 @@
 #version 450
 
 layout(location = 0) in vec3 Vertex_Position;
+layout(location = 1) in vec2 Texture_Coordinates;
 layout(set = 0, binding = 0) uniform Camera {
     mat4 ViewProj;
 };
 layout(set = 1, binding = 0) uniform Transform {
     mat4 Model;
 };
+layout(set = 2, binding = 0) uniform sampler HeightMap;
 
-layout(location = 6) out float origin_distance;
 layout(location = 5) out float v_height;
+layout(location = 6) out float origin_distance;
 
 void main() {
     vec4 Original_Position = Model * vec4(Vertex_Position, 1.0);
-
     origin_distance = sqrt(pow(Original_Position.x, 2) + pow(Original_Position.z, 2));  
-    float h = sin(Original_Position.x) + sin(Original_Position.z);
-    vec4 position = Original_Position + vec4(0, h, 0, 0);
+    vec4 position = Original_Position;
 
     gl_Position = ViewProj * position;
-    v_height = h;
+    v_height = position.y;
 
 }
-
 
 
 
