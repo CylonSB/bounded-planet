f9b38c17f4f74596e5db3e46d99649487a995518
Oscar <osmithsi@sfu.ca>
2020-10-22 11:21:46 -0700

    Refactor egui system node state (Fixes #59)
    Change state to only hold entity handle for the draw entity instead of handles to each individual component.

--- crates/bevy_egui/src/egui_node.rs
+++ crates/bevy_egui/src/egui_node.rs
@@ -90,13 +90,11 @@ impl Node for EguiSystemNode {
         _output: &mut ResourceSlots,
     ) {
         self.command_queue.execute(render_context);
-        println!("Node update!");
     }
 }
 
 impl SystemNode for EguiSystemNode {
     fn get_system(&self, commands: &mut Commands) -> Box<dyn System> {
-        println!("Grabbing EguiSystemNode system!");
         let system = egui_node_system.system();
 
         commands.insert_local_resource(
@@ -108,24 +106,16 @@ impl SystemNode for EguiSystemNode {
     }
 }
 
-// TODO(#59) modify EguiSystemNodeState so that it only holds onto the draw entity and gets the other information each tick from the query
 /// Internal state for the egui system node update system
 struct EguiSystemNodeState {
     /// The egui context this system node is associated with.
     context: Handle<EguiContext>,
     /// The entity associated with the egui context which actually enables drawing of the ui.
     draw_entity: Option<Entity>,
-
     /// The bevy command queue that enables sending render resource manipulation commands.
     command_queue: CommandQueue,
-
-    /// Handle to the mesh that is drawn to display this egui.
-    mesh: Option<Handle<Mesh>>,
     /// The handler which processes updates to the mesh drawn to.
     mesh_handler: MeshHandler,
-
-    /// Handle to the egui node which stores texture data of this egui.
-    egui_node: Option<Handle<EguiNode>>,
 }
 
 impl EguiSystemNodeState {
@@ -134,9 +124,7 @@ impl EguiSystemNodeState {
             context: node.context,
             draw_entity: None,
             command_queue: node.command_queue.clone(),
-            mesh: None,
             mesh_handler: MeshHandler::default(),
-            egui_node: None,
         }
     }
 }
@@ -148,7 +136,6 @@ impl FromResources for EguiSystemNodeState {
 }
 
 /// Update system for a egui system node
-#[allow(clippy::too_many_arguments)]
 #[allow(clippy::type_complexity)]
 fn egui_node_system(
     mut state: Local<EguiSystemNodeState>,
@@ -162,92 +149,56 @@ fn egui_node_system(
 
     mut query: Query<(Entity, &mut EguiJobsDescriptor, &mut Handle<EguiNode>, &mut Handle<Mesh>, &mut RenderPipelines)>,
 ) {
-    println!("Egui node system running!");
-
     // Easier to put the annotation then mess up the nice shorthand with underscores on just the bindings
     #[allow(unused_variables)]
     let EguiSystemNodeState {
         context,
         draw_entity,
         command_queue,
-        mesh,
         mesh_handler,
-        egui_node,
-        // texture_handler,
     } = &mut *state;
 
     let render_resource_context = &**render_resource_context;
-
-    let EguiContext { context, .. } = contexts.get_mut(context).unwrap();
-
-    // If we've not set the draw entity yet, check if there exists the entity.
-    // If it doesn't exist, we return and check again next tick.
-    if draw_entity.is_none() && query.iter().iter().next().is_none() {
-        println!("Leaving early from egui system node update...");
-        return;
-    }
+    let EguiContext { context, .. } = contexts.get_mut(context)
+        .expect("Context handle associated with egui node system isn't valid!");
 
     // Get this context's draw entity, or grab it from the query if we don't yet have it
     let draw_entity = draw_entity.get_or_insert_with(|| {
-        println!("Starting to set draw_entity...");
         let mut query_iter = query.iter();
-        let (entity, ..) = query_iter.iter().next().expect("We already checked that the entity exists just above!");
+        let (entity, ..) = query_iter.iter().next().expect("No egui draw entities seem to exist!");
         
         entity
     });
 
-    // Get the egui node on the draw entity, or grab it from the query if we don't yet have it
-    let egui_node = egui_node.get_or_insert_with(|| {
-        let mut entity_query = query.entity(*draw_entity).unwrap();
-        let (_, _, node, ..) = entity_query.get().unwrap();
+    let mut entity_query = query.entity(*draw_entity)
+        .expect("Unable to make query from egui draw entity! This is a logic error indicating it's somehow been removed or other stuff broke!");
+    let (_, mut jobs_descriptor, egui_node_handle, mesh_handle, render_pipelines) = entity_query.get()
+        .expect("Egui draw entity did not match query! This is a logic error indicating someone has changed the components or removed the entity!");
 
-        *node
-    });
-
-    if let Some(node) = nodes.get_mut(egui_node) {
+    if let Some(node) = nodes.get_mut(&egui_node_handle) {
         let egui_texture = context.texture();
 
         if egui_texture.version != node.texture_hash {
-            println!("Texture has changed since first initialization!");
-            let tex = textures.get_mut(&node.texture).unwrap();
+            let tex = textures.get_mut(&node.texture)
+                .expect("Egui node texture handle is invalid for texture assets!");
 
             tex.size = Vec2::new(egui_texture.width as _, egui_texture.height as _);
             tex.data = egui_texture.pixels.clone();
 
             node.texture_hash = egui_texture.version;
-        } else {
-            println!("Texture has _not_ changed since start!");
         }
     }
 
-    // Get the mesh asset to use for rendering this gui, or create it if it doesn't exist
-    let mesh = mesh.get_or_insert_with(|| {
-        println!("Starting to set mesh...");
-        let mut entity_query = query.entity(*draw_entity).unwrap();
-        let (_, _, _, mesh, ..) = entity_query.get().unwrap();
-
-        *mesh
-    });
-
     let (_output, jobs) = context.end_frame();
 
-    let mut entity_query = query.entity(*draw_entity)
-        .expect("Unable to make query from egui draw entity!");
-    let (_, mut jobs_descriptor, _, _, pipelines) = entity_query.get()
-        .expect("Egui draw entity did not have a RenderPipelines or EguiJobsDescriptor for some reason?");
-
     // TODO(#54): update mesh handler to use staging buffers via command_queue to make this less bad
-    mesh_handler.with_context(render_resource_context, *mesh)
+    mesh_handler.with_context(render_resource_context, *mesh_handle)
         .update_from_jobs(jobs, &mut jobs_descriptor)
-        .set_pipeline_bindings(pipelines);
-
-    println!("Trying to send EguiFrameStart event!");
+        .set_pipeline_bindings(render_pipelines);
 
     // TODO(#60) actually give real input to egui, should require the context to actually be owned by the state update system and sent here rather than the other way around
     // Now that frame update crap is over, begin a new frame and send the Ui object away to be used
     frame_init_event.send(EguiFrameStartEvent {
         new_ui: context.begin_frame(FAKE_RAW_INPUT)
     });
-
-    println!("Sucessfully sent EguiFrameStart event!");
 }
